8c50e92c7473b885c7ff1b65131163ec573df14c
JoshK8BIO <joshkoller8@yahoo.com>
2024-08-05 10:04:17 -0400

    Functions to solve matrix

---
+++ SolveFunctions.R
@@ -0,0 +1,28 @@
+
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) inv <<- solve
+    getsolve <- function() inv
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+}
+
+cacheSolve <- function(x, ...) {
+    inv <- x$getsolve()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setsolve(inv)
+    inv
+}
+
+
